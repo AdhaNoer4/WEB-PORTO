066fae129f7a4b8d526b50bac045a740a4f13fb8
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-11-10 15:23:28 +0700

    2024 | Hero Section 0.1 = make a navbar & profile

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        "pop": ["Poppins", "sans-serif"],
+      },
+      padding: {
+        '9%': '9%',
+      },
+      width: {
+        '32vw':'32vw'
+      }
+  },
+  plugins: [],
+}
+}
