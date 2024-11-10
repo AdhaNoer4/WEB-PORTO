7a70ba5d168c76eed9cadbba536064ab0502c3d9
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-11-10 22:44:42 +0700

    2024 | Hero Section 0.2 = make sosmed link & button hire

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,14 @@ module.exports = {
       },
       width: {
         '32vw':'32vw'
+      },
+      boxShadow: {
+        'boxShadow': '0 0 25px solid rgba(0,0,0,0.1)',
+      },
+      letterSpacing: {
+        'btnsps': '.3em'
       }
+
   },
   plugins: [],
 }
