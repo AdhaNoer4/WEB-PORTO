9a2944283b0811718abaa80ffcda3741adc06c39
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-11-12 21:46:17 +0700

    2024 | Hero Section 0.3 = make a typing text animation

---
+++ tahun-2024/app.js
@@ -0,0 +1,37 @@
+const text = [
+    "Web Developer",
+    "Gamer",
+    "Content Creator",
+    "Web Designer",
+    "Front-End Developer"
+];
+
+let speed = 100;
+const textElements = document.querySelector(".text-writer");
+
+let textIndex = 0;
+let charIndex = 0;
+
+const typeWriter = ()=>{
+    if (charIndex < text[textIndex].length) {
+        textElements.innerHTML += text[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeWriter, speed);
+    } else{
+        setTimeout(eraseText, 1000);
+    }
+
+}
+
+const eraseText = ()=>{
+    if (textElements.innerHTML.length >0 ) {
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    } else{
+        textIndex = (textIndex + 1)% text.length;
+        charIndex = 0;
+        setTimeout(typeWriter,500);
+    }
+}
+
+window.onload = typeWriter
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,38 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "pop": ["Poppins", "sans-serif"],
+        pop: ["Poppins", "sans-serif"],
       },
       padding: {
-        '9%': '9%',
+        "9%": "9%",
       },
       width: {
-        '32vw':'32vw'
+        "32vw": "32vw",
+        calc: "calc(100%+8px)",
       },
       boxShadow: {
-        'boxShadow': '0 0 25px solid rgba(0,0,0,0.1)',
+        boxShadow: "0 0 25px solid rgba(0,0,0,0.1)",
       },
       letterSpacing: {
-        'btnsps': '.3em'
-      }
-
+        btnsps: ".3em",
+      },
+      keyframes: {
+        cursor: {
+          to: { "border-left": "3px solid #f87171" },
+        },
+        words: {
+          "0%, 20%": { content: "Web Developer" },
+          "21%, 40%": { content: "Gamer" },
+          "41%, 60%": { content: "Content Creator" },
+          "61%, 80%": { content: "Web Designer" },
+          "81%, 100%": { content: "Front-End Developer" },
+        },
+      },
+      animation: {
+        words: "words 20s ease-out infinite",
+        cursor: "cursor .6s linear infinite alternate",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-}
+};
