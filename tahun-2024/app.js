6cdf10311bb5ab88707dabfb6c0a9daf4566c381
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-11-18 12:06:31 +0700

    2024 | Hero Section 0.4 = added library particles.js on background

--- tahun-2024/app.js
+++ tahun-2024/app.js
@@ -35,3 +35,10 @@ const eraseText = ()=>{
 }
 
 window.onload = typeWriter
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particle-js', 'assets/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  
