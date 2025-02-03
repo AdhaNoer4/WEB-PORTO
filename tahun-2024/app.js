27b5392eee1cf96803126cd06b5faf451e6034f4
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2025-02-03 11:59:16 +0700

    feat : add function on contact form

--- tahun-2024/app.js
+++ tahun-2024/app.js
@@ -1,10 +1,4 @@
-const text = [
-    "Web Developer",
-    "Gamer",
-    "Content Creator",
-    "Web Designer",
-    "Front-End Developer"
-];
+const text = ["Web Developer", "Gamer", "Content Creator", "Web Designer", "Front-End Developer"];
 
 let speed = 100;
 const textElements = document.querySelector(".text-writer");
@@ -12,33 +6,53 @@ const textElements = document.querySelector(".text-writer");
 let textIndex = 0;
 let charIndex = 0;
 
-const typeWriter = ()=>{
-    if (charIndex < text[textIndex].length) {
-        textElements.innerHTML += text[textIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(typeWriter, speed);
-    } else{
-        setTimeout(eraseText, 1000);
-    }
+const typeWriter = () => {
+  if (charIndex < text[textIndex].length) {
+    textElements.innerHTML += text[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(eraseText, 1000);
+  }
+};
 
-}
+const eraseText = () => {
+  if (textElements.innerHTML.length > 0) {
+    textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+    setTimeout(eraseText, 50);
+  } else {
+    textIndex = (textIndex + 1) % text.length;
+    charIndex = 0;
+    setTimeout(typeWriter, 500);
+  }
+};
 
-const eraseText = ()=>{
-    if (textElements.innerHTML.length >0 ) {
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
-        setTimeout(eraseText, 50)
-    } else{
-        textIndex = (textIndex + 1)% text.length;
-        charIndex = 0;
-        setTimeout(typeWriter,500);
-    }
-}
-
-window.onload = typeWriter
+window.onload = typeWriter;
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particle-js', 'assets/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+particlesJS.load("particle-js", "assets/particlesjs-config.json", function () {
+  console.log("callback - particles.js config loaded");
+});
 
-  AOS.init();
+AOS.init();
+
+// Conttact Form
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  fetch("https://script.google.com/macros/s/AKfycbzFQ55ynciCV6u92-UXCqBcXn0gZBqyEmNdI80IJp-1Y5X_iLqiMcbiGj2OiRRZXI5pfQ/exec", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+    mode: "no-cors"
+  })
+  .then(() => alert("Message sent successfully!"))
+  .catch(error => console.error("Error:", error));
+  
+});
